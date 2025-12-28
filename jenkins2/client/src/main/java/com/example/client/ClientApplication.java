c3fddeb19b365d7cef525764135e242f2eeb1043
iMaDissame <topimad101@gmail.com>
2025-12-28 13:29:25 +0100

    TP32 realise

---
+++ jenkins2/client/src/main/java/com/example/client/ClientApplication.java
@@ -0,0 +1,25 @@
+package com.example.client;
+
+import com.example.client.entities.Client;
+import com.example.client.repositories.ClientRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner initializeH2Database(ClientRepository clientRepository) {
+        return args -> {
+            clientRepository.save(new Client(null, "Amine SAFI", Float.parseFloat("23")));
+            clientRepository.save(new Client(null, "Amal ALAOUI", Float.parseFloat("22")));
+            clientRepository.save(new Client(null, "Samir RAMI", Float.parseFloat("22")));
+        };
+    }
+}
